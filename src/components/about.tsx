428ad6f887e0a29e54ec1599d2483d2d4a8dd2ad
From Sukong <fromsukong@users.noreply.github.com>
2026-08-12 10:48:23 +0000

    feat: remove hero from about page + sticky back bar

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -1,13 +1,11 @@
 import {Theme} from '@astryxdesign/core/theme';
 import {VStack} from '@astryxdesign/core/Layout';
 import {fromsukongTheme} from '../themes/fromsukong';
-import ProfileHeader from './ProfileHeader';
 import InfoSection from './InfoSection';
 
 export default function AboutPage() {
   return (
     <Theme theme={fromsukongTheme} mode="dark">
-      <ProfileHeader />
       <VStack className="page">
         <VStack className="section-gap">
           <InfoSection />
